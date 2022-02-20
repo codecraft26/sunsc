f4dbba708a0860d7ec47bec30eb6f1bf7346f06f
codecraft26 <codescraft26@gmail.com>
2022-02-20 12:17:57 +0530

    adding dependencies

--- app/src/main/java/dev/aman/sunsc/MainActivity.kt
+++ app/src/main/java/dev/aman/sunsc/MainActivity.kt
@@ -2,7 +2,7 @@ package dev.aman.sunsc
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-
+// helo
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
