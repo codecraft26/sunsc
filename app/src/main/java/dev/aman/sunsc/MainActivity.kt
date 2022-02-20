25798265bc3c5ff1294adcdc1c588300d7f8a9d9
codecraft26 <codescraft26@gmail.com>
2022-02-20 09:13:18 +0530

    initial commit

--- app/src/main/java/dev/aman/sunsc/MainActivity.kt
+++ app/src/main/java/dev/aman/sunsc/MainActivity.kt
@@ -2,7 +2,7 @@ package dev.aman.sunsc
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-
+// helo world
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
