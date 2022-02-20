ad85362149440ab832b5bd2d35f40a9463592cff
codecraft26 <codescraft26@gmail.com>
2022-02-20 12:28:16 +0530

    adding firebase

---
+++ app/src/main/java/dev/aman/sunsc/data/entities/Songs.kt
@@ -0,0 +1,9 @@
+package dev.aman.sunsc.data.entities
+
+ data class Songs (
+     val mediaId: String = "",
+     val title: String = "",
+     val subtitle: String = "",
+     val songUrl: String = "",
+     val imageUrl: String = ""
+         )
